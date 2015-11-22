be25be33ff789a2b97531b691d2e491bf3251d20
Mesut Evin <mesut@mesutevin.net>
2015-11-22 02:29:28 +0200

    Blink LED example is tested.

--- main.c
+++ main.c
@@ -1,26 +1,20 @@
 #include "hal.h"
 #include "ch.h"
-#include "math.h"
-
-#define N 16
-
-
+#include "test.h"
 
 int main(void)
 {
 	halInit();
 	chSysInit();
-	palSetPadMode(GPIOA, 3, PAL_MODE_ALTERNATE(7));
-	palSetPadMode(GPIOA, 2, PAL_MODE_ALTERNATE(7));
-	palSetPadMode(GPIOD, 12, PAL_MODE_OUTPUT_PUSHPULL);
-	palSetPadMode(GPIOD, 13, PAL_MODE_OUTPUT_PUSHPULL);
-	palSetPadMode(GPIOD, 14, PAL_MODE_OUTPUT_PUSHPULL);
-	palSetPadMode(GPIOD, 15, PAL_MODE_OUTPUT_PUSHPULL);
-	/*usart2 ve ledler kullanıma hazır*/
-
-	while(true){
-	}
-
+	
+	
+	while(TRUE){
+	
+		palSetPad(GPIOD, GPIOD_LED3);       /* Orange.  */
+		chThdSleepMilliseconds(500);
+		palClearPad(GPIOD, GPIOD_LED3);     /* Orange.  */
+		chThdSleepMilliseconds(500);
  
+	}
 	return 0;
 }
